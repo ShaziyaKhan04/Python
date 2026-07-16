d974f514425031b63975e8a601fe6d762dfc8b89
Shaziyakhan04 <shaziyafkhan0516@gmail.com>
2026-07-16 11:21:44 +0530

    Student grade system

---
+++ Student_grade_system.py
@@ -0,0 +1,12 @@
+Marks = int(input("Enter your marks:"))
+if Marks >= 90:
+    print("A+")
+elif Marks >= 80 and Marks < 90:
+    print("A")
+elif Marks >= 70 and Marks < 80:
+    print("B")
+elif Marks >= 60 and Marks < 70:
+    print("C")
+else:
+    print("Fail")
+    
